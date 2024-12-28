a8050facd1f8f6862e6d9aebb55fc8eefee5a03f
enskerti <enskerti@gmail.com>
2024-12-28 14:38:00 +0300

    drivers with for loops added

--- src/test/java/steps/getInformations.java
+++ src/test/java/steps/getInformations.java
@@ -1,13 +1,57 @@
 package steps;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.page.Page;
+import org.openqa.selenium.interactions.Actions;
 import utillities.TestBase;
+
+import java.util.List;
 
 public class getInformations extends TestBase {
 
     @Test
-    public void newArrivals() {
+    public void newArrivals() throws InterruptedException {
+        WebElement newArrivalsLink = driver.findElement(By.xpath("(//a[@href='/new_arrivals/'])[1]"));
+        newArrivalsLink.click();
 
+        WebElement allBtn = driver.findElement(By.xpath("//span[text()='ALL']"));
+        allBtn.click();
+
+        List<WebElement> productList = driver.findElements(By.xpath("//div[@id='styleNumBox']//div[@class='col position-relative']"));
+
+        for (int i = 0; i < productList.size(); i++) {
+            WebElement product = productList.get(i);
+            product.click();
+            Thread.sleep(1000);
+
+            boolean isContainsCollection = driver.findElement(By.xpath("//li[@class='breadcrumb-item active']")).getText().contains("Collection");
+            System.out.println(isContainsCollection);
+            if (isContainsCollection){
+                List<WebElement> subProductList = driver.findElements(By.xpath("//div[@class='row mt-3 row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5']" +
+                        "//div[@class='mt-3 text-center thumb-item-box thumb-border quick-view-box lh-sm']"));
+                for (int j = 0; j < subProductList.size(); j++) {
+                    // Listeyi yeniden alın
+                    List<WebElement> subProductListInLoop = driver.findElements(By.xpath("//div[@class='row mt-3 row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5']" +
+                            "//div[@class='mt-3 text-center thumb-item-box thumb-border quick-view-box lh-sm']//img"));
+                    System.out.println(subProductListInLoop.size());
+                    WebElement subProduct = subProductListInLoop.get(j);
+
+                    // Tıklayın
+                    subProduct.click();
+                    Thread.sleep(1000);
+
+                    // Geri dön
+                    driver.navigate().back();
+                    Thread.sleep(1000);
+                }
+
+            }
+            driver.navigate().back();
+            Thread.sleep(3000);
+        }
+        System.out.println(productList.size());
     }
 
     @Test
--- src/test/java/utillities/TestBase.java
+++ src/test/java/utillities/TestBase.java
@@ -2,8 +2,12 @@ package utillities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -12,16 +16,27 @@ public class TestBase {
 
     protected static WebDriver driver;
 
-    @Before
-    public void setup() {
+    @BeforeClass
+    public static void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get(ConfigReader.getProperty("loginUrl"));
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        usernameInput.sendKeys(ConfigReader.getProperty("userName"));
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys(ConfigReader.getProperty("password"));
+
+        WebElement signInBtn = driver.findElement(By.id("submitBtn"));
+        signInBtn.click();
     }
 
-    @After
-    public void teardown() {
+    @AfterClass
+    public static void teardown() {
         driver.quit();
     }
 }
