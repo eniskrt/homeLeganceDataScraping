fb38da21d10839a47031bb70d0fd9d306dfeadf8
enskerti <enskerti@gmail.com>
2024-12-28 04:04:26 +0300

    initial commit

---
+++ src/test/java/steps/getInformations.java
@@ -0,0 +1,67 @@
+package steps;
+
+import org.junit.Test;
+import utillities.TestBase;
+
+public class getInformations extends TestBase {
+
+    @Test
+    public void newArrivals() {
+
+    }
+
+    @Test
+    public void bedroom() {
+
+    }
+
+    @Test
+    public void dining() {
+
+    }
+
+    @Test
+    public void youth() {
+
+    }
+
+    @Test
+    public void seating() {
+
+    }
+
+    @Test
+    public void occasional() {
+
+    }
+
+    @Test
+    public void office() {
+
+    }
+
+    @Test
+    public void accent() {
+
+    }
+
+    @Test
+    public void media() {
+
+    }
+
+    @Test
+    public void lighting() {
+
+    }
+
+    @Test
+    public void mattress() {
+
+    }
+
+    @Test
+    public void onSale() {
+
+    }
+}
